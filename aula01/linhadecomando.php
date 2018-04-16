b202040b14a563cfc41360e25bbeb45b25007995
hamiltonvalerio <hamilton@valerio.eti.br>
2018-04-16 14:50:32 -0300

    estrutura de decisao

--- aula01/linhadecomando.php
+++ aula01/linhadecomando.php
@@ -1,11 +1,49 @@
 <?php 
-	
-	$nome = readline('Digite seu nome: ');
+	echo '----------';
+	echo "\n";
+	echo '-0 - Soma-';
+	echo "\n";
+	echo '-1 - Subt-';
+	echo "\n";
+	echo '-2 - Mult-';
+	echo "\n";
+	echo '-3 - Divi-';
+	echo "\n";
+	$opcao = readline('Escolha uma opcao: ');
 
-	if($nome == 'Adriano'){
-		
-		echo 'Juruna';
-		echo "\n";
+	switch ($opcao) {
+		case 0:
+			$num1 = readline('Primeiro Numero: ');
+			$num2 = readline('Segundo Numero: ');
+			echo 'Soma: '.($num1+$num2);
+			echo "\n";
+			break;
+		case 1:
+			$num1 = readline('Primeiro Numero: ');
+			$num2 = readline('Segundo Numero: ');
+			echo 'Subt: '.($num1-$num2);
+			echo "\n";
+			break;
+		case 2:
+			$num1 = readline('Primeiro Numero: ');
+			$num2 = readline('Segundo Numero: ');
+			echo 'MUlt: '.($num1*$num2);
+			echo "\n";
+			break;
+		case 3:
+			$num1 = readline('Primeiro Numero: ');
+			$num2 = readline('Segundo Numero: ');
+			echo 'Divi: '.($num1/$num2);
+			echo "\n";
+			break;
+		default:
+			echo 'errou';
+			echo "\n";
+			break;
 	}
+
+	//---------------------
+
+	
 
  ?>
