733259e52c14f915ef0d2a68852f17d86354c513
hamiltonvalerio <hamilton@valerio.eti.br>
2018-04-18 17:05:00 -0300

    aula

---
+++ aula03/pessoas/index.php
@@ -0,0 +1,14 @@
+<ul>
+	<li>
+		<a href="pessoa.php?nome=Hamilton">Hamilton</a>
+	</li>
+	<li>
+		<a href="pessoa.php?nome=Graziela">Graziela</a>
+	</li>
+	<li>
+		<a href="pessoa.php?nome=Oliver">Oliver</a>
+	</li>
+	<li>
+		<a href="pessoa.php?nome=Valentina">Valentina</a>
+	</li>
+</ul>
---
+++ aula03/pessoas/pessoa.php
@@ -0,0 +1,6 @@
+<a href="index.php">Voltar</a>
+<?php 
+
+	echo '<p>Olá ' . $_GET['nome'] .'</p>';
+
+ ?>
